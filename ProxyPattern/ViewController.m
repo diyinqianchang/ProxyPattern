b8502c80dcac9fe0040a0936188bd23daab5726c
YouXianMing <705786299@qq.com>
2015-08-04 21:54:01 +0800

    1.0

--- ProxyPattern/ViewController.m
+++ ProxyPattern/ViewController.m
@@ -1,27 +1,35 @@
 //
 //  ViewController.m
-//  ProxyPattern
+//  AppProxy
 //
 //  Created by YouXianMing on 15/8/4.
 //  Copyright (c) 2015年 YouXianMing. All rights reserved.
 //
 
 #import "ViewController.h"
+#import "MessageProtocol.h"
+#import "ConcreteProxy.h"
 
-@interface ViewController ()
+@interface ViewController () <MessageProtocol>
+
+@property (nonatomic, strong) ConcreteProxy  *proxy;
 
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.proxy = [[ConcreteProxy alloc] initWithCustomer:self];
+    [self.proxy helloWorld];
+    [self.proxy goodBye];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)helloWorld {
+    
+    NSLog(@"helloWorld");
 }
 
 @end
